2d8855d5a8cedfe391dcad4384f3c023553206c3
Mihir Patel <mvpatel2000@yahoo.com>
2016-02-06 19:25:00 -0500

    Registration Draft
    Registration Part 1

--- app/src/main/java/com/example/mihir/haloworld/LoginActivity.java
+++ app/src/main/java/com/example/mihir/haloworld/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.mihir.haloworld;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -342,6 +343,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(i);
                 finish();
             } else {
                 mPhoneNumber.setError(getString(R.string.error_incorrect_password));
